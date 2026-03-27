55ae06194e34f04cb979741101ec04a03cbc5a44
samsara <1260159878@qq.com>
2026-03-27 11:11:19 +0800

    格式化 2_1_rdma_init_test.c，调整代码风格以提高可读性

--- src/2_1_rdma_init_test.c
+++ src/2_1_rdma_init_test.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <infiniband/verbs.h> // 必须包含这个头文件
 
-int main() {
+int main()
+{
     struct ibv_device **dev_list;
     struct ibv_context *ctx;
     struct ibv_pd *pd;
@@ -11,45 +12,56 @@ int main() {
     struct ibv_qp_init_attr qp_init_attr;
 
     // 1. 找网卡 (Get Device List)
+    // 这一步之后，驱动程序会在内核里为你这个进程创建一个 Context。
+    // 通过这个 Context，你的程序获得了直接访问网卡硬件寄存器的“通行证”。
     dev_list = ibv_get_device_list(NULL);
-    if (!dev_list) {
+    if (!dev_list)
+    {
         perror("Failed to get IB devices list");
         return 1;
     }
 
     // 2. 开网卡 (Open Device) - 假设用列表里的第一个
     ctx = ibv_open_device(dev_list[0]);
-    if (!ctx) {
+    if (!ctx)
+    {
         fprintf(stderr, "Couldn't open device %s\n", ibv_get_device_name(dev_list[0]));
         return 1;
     }
     printf("成功打开网卡: %s\n", ibv_get_device_name(dev_list[0]));
 
     // 3. 分配 PD (Alloc PD) - 你的隔离房间
+    // 你在网卡硬件里划出了一块“私有领地”。以后你注册的内存（MR）和创建的 QP 只要填入这个 PD 句柄，
+    // 硬件就会自动识别它们属于同一个保护域。
     pd = ibv_alloc_pd(ctx);
-    if (!pd) {
+    if (!pd)
+    {
         fprintf(stderr, "Couldn't allocate PD\n");
         return 1;
     }
 
     // 4. 创建 CQ (Create CQ) - 你的收件箱，深度设为 100
     cq = ibv_create_cq(ctx, 100, NULL, NULL, 0);
-    if (!cq) {
+    if (!cq)
+    {
         fprintf(stderr, "Couldn't create CQ\n");
         return 1;
     }
 
     // 5. 创建 QP (Create QP) - 雇佣搬运工
+    // 当你调用 create_qp 时，网卡会真的在自己的片上内存
+    // 或者 host 内存里开辟一段空间来管理这个 QP 的状态。
     memset(&qp_init_attr, 0, sizeof(qp_init_attr));
     qp_init_attr.send_cq = cq;
     qp_init_attr.recv_cq = cq;
     qp_init_attr.qp_type = IBV_QPT_RC; // 可靠连接类型
-    qp_init_attr.cap.max_send_wr = 10;  // 发送队列深度
-    qp_init_attr.cap.max_recv_wr = 10;  // 接收队列深度
+    qp_init_attr.cap.max_send_wr = 10; // 发送队列深度
+    qp_init_attr.cap.max_recv_wr = 10; // 接收队列深度
     qp_init_attr.cap.max_send_sge = 1; // 每次搬运允许碎片的个数
 
     qp = ibv_create_qp(pd, &qp_init_attr);
-    if (!qp) {
+    if (!qp)
+    {
         fprintf(stderr, "Couldn't create QP\n");
         return 1;
     }
